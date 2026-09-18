693061f00ea1a046e8921e8d646c982a09e3f644
rezvo85-cmd <rezvo85@gmail.com>
2026-09-18 01:23:08 -0700

    Refresh RONN PWA cache for R14

--- RONN_CORE_RENDER_DEPLOY_V1/_ronn/static/service-worker.js
+++ RONN_CORE_RENDER_DEPLOY_V1/_ronn/static/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="ronn-assets-r13";
+const CACHE="ronn-assets-r14";
 const ASSETS=["/static/ronn_app_icon.png","/manifest.webmanifest"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
