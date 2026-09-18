ec3dabb5ccbde3bb35ce98ba09baf27ac2e617eb
rezvo85-cmd <rezvo85@gmail.com>
2026-09-18 05:08:01 -0700

    Refresh VIEWPORT6 mobile cache

--- RONN_CORE_RENDER_DEPLOY_V1/_ronn/static/service-worker.js
+++ RONN_CORE_RENDER_DEPLOY_V1/_ronn/static/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="ronn-assets-r20-composer5";
-const ASSETS=["/static/ronn_home_icon_v2.png","/static/ronn_logo.svg?v=RONN-R19-LOGO2","/static/style.css?v=RONN-R19-CLEAN1","/static/mobile_fit.css?v=RONN-R20-COMPOSER5","/static/app.js?v=RONN-R20-COMPOSER5","/manifest.webmanifest?v=RONN-R19-MOBILE2"];
+const CACHE="ronn-assets-r20-viewport6";
+const ASSETS=["/static/ronn_home_icon_v2.png","/static/ronn_logo.svg?v=RONN-R19-LOGO2","/static/style.css?v=RONN-R19-CLEAN1","/static/mobile_fit.css?v=RONN-R20-VIEWPORT6","/static/app.js?v=RONN-R20-VIEWPORT6","/manifest.webmanifest?v=RONN-R19-MOBILE2"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
   self.skipWaiting();
