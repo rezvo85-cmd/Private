edc4e5c659141efcae59e27320a9437fbb1b0119
rezvo85-cmd <rezvo85@gmail.com>
2026-09-25 17:31:30 -0700

    Rotate RONN web cache after Roblox bridge protocol update

--- RONN_CORE_RENDER_DEPLOY_V1/_ronn/static/service-worker.js
+++ RONN_CORE_RENDER_DEPLOY_V1/_ronn/static/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="ronn-assets-r23-roblox-mcp1";
-const ASSETS=["/static/ronn_home_icon_v2.png","/static/ronn_logo.svg?v=RONN-R19-LOGO2","/static/style.css?v=RONN-R23-ROBLOX-MCP1","/static/mobile_fit.css?v=RONN-R23-ROBLOX-MCP1","/static/app.js?v=RONN-R23-ROBLOX-MCP1","/manifest.webmanifest?v=RONN-R23-ROBLOX-MCP1"];
+const CACHE="ronn-assets-r23-roblox-mcp2";
+const ASSETS=["/static/ronn_home_icon_v2.png","/static/ronn_logo.svg?v=RONN-R19-LOGO2","/static/style.css?v=RONN-R23-ROBLOX-MCP2","/static/mobile_fit.css?v=RONN-R23-ROBLOX-MCP2","/static/app.js?v=RONN-R23-ROBLOX-MCP2","/manifest.webmanifest?v=RONN-R23-ROBLOX-MCP2"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
   self.skipWaiting();
